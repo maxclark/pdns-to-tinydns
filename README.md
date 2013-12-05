1d99e965d60605aa5b12906f36be38543ebe5b80
Max Clark <max@clarksys.com>
2013-12-04 16:43:53 -0800

    Merge of text/number escaping by Anders Brownworth & Open Collaborative Communities Network

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# pdns-to-tinydns.pl: a tool to generate a tinydns data.cdb file from a powerdns database.
+# pdns-to-tinydns
+
+A tool to generate a tinydns data.cdb file from a powerdns database.
+
+by [Max Clark](http://www.clarksys.com)
 
 ## Download
 
-https://github.com/maxclark/pdns-to-tinydns
+[https://github.com/maxclark/pdns-to-tinydns][1]
+
+## Credits
+
+[Anders Brownworth][2] [djbdns/tinydns Record Builder][3]
+[Open Collaborative Communities Network][4] [http://occnetwork.net/pdns-to-djbdns.txt][5]
 
 ## License
 
 pdns-to-tinydns is released under the GPL license. See the file COPYING included in the distribution for details.
+
+[1]: https://github.com/maxclark/pdns-to-tinydns
+[2]: http://anders.com/
+[3]: http://anders.com/projects/sysadmin/djbdnsRecordBuilder/
+[4]: http://occnetwork.net
+[5]: http://occnetwork.net/pdns-to-djbdns.txt
--- pdns-to-tinydns.pl
+++ pdns-to-tinydns.pl
@@ -24,6 +24,7 @@ $database = "";
 $hostname = "";
 $username = "";
 $password = "";
+$tinydns = "/etc/tinydns";
 
 # Load necessary perl modules
 # --
@@ -58,9 +59,9 @@ $records_sth = $dbh->prepare(q(SELECT name, type, content, ttl, prio
 
 # Move the old file out of the way and create the filehandle
 # --
-system("mv /usr/local/tinydns/root/data /usr/local/tinydns/root/data.old");
+system("mv $tinydns/root/data $tinydns/root/data.old");
 
-open DATA, ">/usr/local/tinydns/root/data" or die $!;
+open DATA, ">$tinydns/root/data" or die $!;
 
 # Get the list of domains, loop through them and grab the records
 # --
@@ -72,31 +73,219 @@ while ( @row = $domains_sth->fetchrow_array ) {
 
 	while ( ( $name, $type, $content, $ttl, $prio ) = $records_sth->fetchrow_array ) {
 
+		# NAME, TTL and PRIO shouldn't have spaces, if they do strip them.
+		# --
 		$name =~ s/\s//g;
-		#$content =~ s/\s//g;
 		$ttl =~ s/\s//g;
 		$prio =~ s/\s//g;
 
-		$ttl = '86400';
+		# Set a minimum and maximum TTL value, this does not impact SOA & NS records
+		# --
+		if ($ttl > 86400) {
+			$ttl = '86400';
+		}
+		elsif ($ttl < 300) {
+			$ttl = '300'
+		}
 
-		if ( $type eq "NS" ) {
-			print DATA ".$name\:\:$content\:259200\n";
+		# SOA and NS can be split using "Z" & "&" record types, or combined using just the "."
+		# --
+		if ( $type eq "SOA" ) {
+
+			# Increase the TTL to 259200
+			# --
+			$ttl = '259200';
+
+			$content =~ s/([^ ]+) ([^@]+)@([^ ]+) (\d+) (\d+) (\d+) (\d+) (\d+)/$1/;
+			$contact = $2 . '.' . $3;
+			$serial  = $4;
+			$refresh = $5;
+			$retry   = $6;
+			$expire  = $7;
+			$min     = $8;
+
+			$string = 'Z'
+				. escapeText( $name ) . ':'
+				. escapeText( $content ) . ':'
+				. escapeText( $contact ) . ':'
+				. $serial . ':'
+				. $refresh . ':'
+				. $retry . ':'
+				. $expire . ':'
+				. $min . ':'
+				. $ttl;
+
+			print DATA "$string\n";
+
 		}
-		if ( $type eq "A" ) {
-			print DATA "+$name\:$content\:$ttl\n";
+		elsif ( $type eq "NS" ) {
+			# Increase the TTL to 259200
+			# --
+			$ttl = '259200';
+
+			$string	= '&'
+				. escapeText($name) . '::'
+				. escapeText($content) . ':'
+				. $ttl;
+
+			print DATA "$string\n";
 		}
-		if ( $type eq "CNAME" ) {
-			print DATA "C$name\:$content\:$ttl\n";
+		elsif ( $type eq "A" ) {
+			$string = '+'
+				. escapeText($name) . ':'
+				. escapeText($content) . ':'
+				. $ttl;
+
+			print DATA "$string\n";
 		}
-		if ( $type eq "MX" ) {
-			print DATA "\@$name\:\:$content\:$prio\:$ttl\n";
+		elsif ( $type eq "CNAME" ) {
+			$string = 'C'
+				. escapeText($name) . ':'
+				. escapeText($content) . ':'
+				. $ttl;
+
+			print DATA "$string\n";
 		}
-		if ( $type eq "PTR" ) {
-			print DATA "\^$name\:$content\:$ttl\n";
+		elsif ( $type eq "MX" ) {
+			$string = '@'
+				. escapeText($name) . '::'
+				. escapeText($content) . ':'
+				. $prio . ':'
+				. $ttl;
+
+			print DATA "$string\n";
 		}
-		if ( $type eq "TXT" ) {
-			$content =~ s/(\W)/sprintf "\\%03o", ord $1/ge;
-			print DATA "\'$name\:$content\:$ttl\n";
+		elsif ( $type eq "PTR" ) {
+			$string = '^'
+				. escapeText($name) . ':'
+				. escapeText($content) . ':'
+				. $ttl;
+
+			print DATA "$string\n";
+		}
+		elsif ( $type eq "TXT" ) {
+			$string = "\'"
+				. escapeText($name) . ':'
+				. escapeText($content) . ':'
+				. $ttl;
+
+			print DATA "$string\n";
+		}
+		elsif ( $type eq "SPF" ) {
+			$string = ":"
+				. escapeText($name) . ":16:"
+            	. characterCount($content)
+            	. escapeText($content) . ":"
+            	. $ttl;
+
+            print DATA "$string\n";
+		}
+		elsif ( $type eq "SRV" ) {
+			# :sip.tcp.example.com:33:\000\001\000\002\023\304\003pbx\007example\003com\000
+			if ( ( $prio >= 0 && $prio <= 65535 ) && ( $weight >= 0 && $weight <= 65535 ) && ( $port >= 0 && $port <= 65535 ) ) {
+            	my $target = "";
+            	my @chunks = split /\./, $content;
+            	foreach my $chunk (@chunks) {
+					$target = $target . characterCount($chunk) . $chunk;
+            	}
+            	$string = ":"
+                . escapeText($name) . ":33:"
+                . escapeNumber($prio)
+                . escapeNumber($weight)
+                . escapeNumber($port)
+                . $target . "\\000" . ":"
+                . $ttl;
+
+                print DATA "$string\n";
+			}
+			else {
+				print "priority, weight or port not within 0 - 65535\n";
+			}
+		}
+		# Not currently supported by the schema, here for reference for future use
+		# --
+		# elsif ($type eq "NAPTR" ) {
+		# 	# :comunip.com:35:\000\012\000\144\001u\007E2U+sip\036!^.*$!sip\072info@comunip.com.br!\000:300
+		# 	#                 |-order-|-pref--|flag|-services-|---------------regexp---------------|re-|
+		# 	if ( ( $$order >= 0 && $order <= 65535 ) && ( $prefrence >= 0 && $prefrence <= 65535 ) ) {
+		# 		$string = ":"
+		# 			. escapeText($name) . ":35:"
+		# 			. escapeNumber($order)
+		# 			. escapeNumber($prefrence)
+		# 			. characterCount($flag)
+		# 			. $flag
+		# 			. characterCount($services)
+		# 			. escapeText($services)
+		# 			. characterCount($regexp)
+		# 			. escapeText($regexp);
+
+		# 		if ( $replacement ne "" ) {
+		# 			$result = $result . characterCount($replacement) . escapeText($replacement);
+		# 		}
+
+		# 		$string = $string . "\\000:" . $cgi{'ttl'};
+
+		# 		print DATA "$string\n";
+		# 	}
+		# 	else {
+		# 		 print "order or prefrence not within 0 - 65535\n";
+		# 	}
+		# }
+		# elsif ( $type eq "domainKeys" ) {
+		# 	# :joe._domainkey.anders.com:16:\341k=rsa; p=MIGfMA0GCSqGSIb3DQ ... E2hHCvoVwXqyZ/MbQIDAQAB
+		# 	#                               |lt|  |typ|  |-key----------------------------------------|
+		# 	if ( $record{'key'} ne "" ) {
+		# 		my $key = $record{'key'};
+		# 		$key =~ s/\r//g;
+		# 		$key =~ s/\n//g;
+		# 		my $line = "k=" . $record{'encryptionType'} . "; p=" . $key;
+
+		# 		$string = ":"
+		# 			. escapeText( $record{'domain'} ) . ":16:"
+		# 			. characterCount($line)
+		# 			. escapeText($line) . ":"
+		# 			. $record{'ttl'};
+
+		# 		print DATA "$string\n";
+		# 	}
+		# 	else {
+		# 		print "didn't get a valid key for the key field\n";
+		# 	}
+		# }
+		elsif ( $type eq "AAAA" ) {
+			# ffff:1234:5678:9abc:def0:1234:0:0
+			# :example.com:28:\377\377\022\064\126\170\232\274\336\360\022\064\000\000\000\000
+			if ( $content ne "" && $name ne "" ) {
+				$colons = $content =~ tr/:/:/;
+				if ($colons < 7) { $content =~ s/::/':' x (9-$colons)/e; }
+				( $a, $b, $c, $d, $e, $f, $g, $h ) = split /:/, $content;
+				if ( ! defined $h ) {
+					print "Didn't get a valid-looking IPv6 address\n";
+				}
+				else {
+					$a = escapeHex( sprintf "%04s", $a );
+					$b = escapeHex( sprintf "%04s", $b );
+					$c = escapeHex( sprintf "%04s", $c );
+					$d = escapeHex( sprintf "%04s", $d );
+					$e = escapeHex( sprintf "%04s", $e );
+					$f = escapeHex( sprintf "%04s", $f );
+					$g = escapeHex( sprintf "%04s", $g );
+					$h = escapeHex( sprintf "%04s", $h );
+
+					$string = ":"
+						. escapeText($name) . ":28:"
+						. "$a$b$c$d$e$f$g$h" . ":"
+						. $ttl;
+
+					print DATA "$string\n";
+            	}
+			}
+			else {
+				print "didn't get a valid address or domain\n";
+			}
+    	}
+		else {
+			print "didn't get a valid record type\n";
 		}
 
 	}
@@ -125,5 +314,55 @@ close DATA;
 
 # "Make" the .cdb using the tinydns provided Makefile
 # --
-chdir("/usr/local/tinydns/root");
+chdir("$tinydns/root");
 system("make");
+
+#--
+sub escapeText {
+	my $line = pop @_;
+	my $out;
+	my @chars = split //, $line;
+
+	foreach $char ( @chars ) {
+		if ( $char =~ /[\r\n\t: \\\/]/ ) {
+			$out = $out . sprintf "\\%.3lo", ord $char;
+		}
+		else {
+			$out = $out . $char;
+		}
+	}
+	return( $out );
+}
+
+sub escapeNumber {
+	my $number = pop @_;
+	my $highNumber = 0;
+
+	if ( $number - 256 >= 0 ) {
+		$highNumber = int( $number / 256 );
+		$number = $number - ( $highNumber * 256 );
+	}
+	$out = sprintf "\\%.3lo", $highNumber;
+	$out = $out . sprintf "\\%.3lo", $number;
+
+	return( $out );
+}
+
+sub escapeHex {
+	# takes a 4 character hex value and converts it to two excaped numbers
+	my $line = pop @_;
+	my @chars = split //, $line;
+
+	$out = sprintf "\\%.3lo", hex "$chars[0]$chars[1]";
+	$out = $out . sprintf "\\%.3lo", hex "$chars[2]$chars[3]";
+
+	return( $out );
+}
+
+sub characterCount {
+	my $line = pop @_;
+	my @chars = split //, $line;
+	my $count = @chars;
+
+	return( sprintf "\\%.3lo", $count );
+}
